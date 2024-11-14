3dc8c0b3d3471e091f2b6d619aedb40b989a12e2
sebastienDOC <s.dechand21@gmail.com>
2024-11-14 14:01:04 +0100

    feat(session): add session page and routing

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { AboutComponent } from "../../components/about/about.component";
-import { BannerComponent } from "../../components/banner/banner.component";
-import { HeaderComponent } from "../../components/header/header.component";
-import { MainBannerComponent } from "../../components/main-banner/main-banner.component";
-import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
 import { HealthcareComponent } from '../../containers/healthcare/healthcare.component';
+import { AboutComponent } from "./components/about/about.component";
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, MainBannerComponent, AboutComponent, TestimonialsComponent, HealthcareComponent],
+  imports: [AboutComponent, TestimonialsComponent, HealthcareComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
