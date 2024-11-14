ea2f8ca475bfa918378ac99c270a6331f3b3f9c2
sebastienDOC <s.dechand21@gmail.com>
2024-11-14 17:04:56 +0100

    style(header): change position fixed

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HealthcareComponent } from '../../containers/healthcare/healthcare.component';
 import { AboutComponent } from "./components/about/about.component";
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { MainBannerComponent } from '../../components/main-banner/main-banner.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [AboutComponent, TestimonialsComponent, HealthcareComponent],
+  imports: [AboutComponent, TestimonialsComponent, HealthcareComponent, MainBannerComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
