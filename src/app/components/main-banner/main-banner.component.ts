2516e52d9581afe0782d6d533e707f87fb7eca47
Rayms <rayms.dev@gmail.com>
2024-11-14 07:55:29 +0100

    feat(external-link): add booking link
    * remove not implemented link buttons

--- src/app/components/main-banner/main-banner.component.ts
+++ src/app/components/main-banner/main-banner.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { EXTERNAL_URL } from '../../configs';
 
 @Component({
   selector: 'app-main-banner',
   standalone: true,
   imports: [],
   templateUrl: './main-banner.component.html',
-  styleUrl: './main-banner.component.scss'
+  styleUrl: './main-banner.component.scss',
 })
 export class MainBannerComponent {
-
+  public bookingUrl: string = EXTERNAL_URL['BOOKING'];
 }
---
+++ src/app/configs/app.constants.ts
@@ -0,0 +1,4 @@
+export const EXTERNAL_URL: Record<string, string> = {
+  BOOKING:
+    'https://perfactive.fr/magnetiseur/saint-leger-triey/jean-luc-laffuge',
+};
