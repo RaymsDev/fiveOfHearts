1b850462fe013616b0827731ee4c40776ff14449
sebastienDOC <s.dechand21@gmail.com>
2024-11-14 11:02:23 +0100

    style(homepage): change global style

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -4,12 +4,12 @@ import { BannerComponent } from "../../components/banner/banner.component";
 import { HeaderComponent } from "../../components/header/header.component";
 import { MainBannerComponent } from "../../components/main-banner/main-banner.component";
 import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
-import { ServicesComponent } from '../services/services.component';
+import { HealthcareComponent } from '../../containers/healthcare/healthcare.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, MainBannerComponent, ServicesComponent, AboutComponent, TestimonialsComponent],
+  imports: [HeaderComponent, BannerComponent, MainBannerComponent, AboutComponent, TestimonialsComponent, HealthcareComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
