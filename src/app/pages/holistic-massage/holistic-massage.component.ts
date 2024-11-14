82687ababd0136221adbf4541bdd83af0e6d8764
sebastienDOC <s.dechand21@gmail.com>
2024-11-14 14:28:34 +0100

    feat(holistic-massage): add session and holistic-massage pages

---
+++ src/app/pages/holistic-massage/holistic-massage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-holistic-massage',
+  standalone: true,
+  imports: [],
+  templateUrl: './holistic-massage.component.html',
+  styleUrl: './holistic-massage.component.scss'
+})
+export class HolisticMassageComponent {
+
+}
