99083a4100a75cd970fe1a0194a6aa050b4e4f80
Rayms <rayms.dev@gmail.com>
2024-11-14 18:25:44 +0100

    fix(SSR): browser refs in SSR

--- src/app/services/script-loader.service.ts
+++ src/app/services/script-loader.service.ts
@@ -1,19 +1,32 @@
-import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Inject,
+  Injectable,
+  PLATFORM_ID,
+  Renderer2,
+  RendererFactory2,
+} from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ScriptLoaderService {
   private renderer: Renderer2;
-
-  constructor(rendererFactory: RendererFactory2) {
+  private platformId: Object;
+  constructor(
+    rendererFactory: RendererFactory2,
+    @Inject(PLATFORM_ID) platformId: Object,
+  ) {
+    this.platformId = platformId;
     this.renderer = rendererFactory.createRenderer(null, null);
   }
 
   loadScript(src: string, defer: boolean = true): void {
-    const script = this.renderer.createElement('script');
-    script.src = src;
-    script.defer = defer;
-    this.renderer.appendChild(document.body, script);
+    if (isPlatformBrowser(this.platformId)) {
+      const script = this.renderer.createElement('script');
+      script.src = src;
+      script.defer = defer;
+      this.renderer.appendChild(document.body, script);
+    }
   }
 }
