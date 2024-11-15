b13b54471d72cd1ca5c58ba96a63f8ac07610f18
sebastienDOC <s.dechand21@gmail.com>
2024-11-15 14:54:10 +0100

    feat(healthcare): add page with healthcare

---
+++ src/app/models/healthcare.model.ts
@@ -0,0 +1,9 @@
+export interface Healthcare {
+  id: number
+  title: string
+  description: string
+  img: string
+  duration: string
+  prices: string
+  audience: string
+}
--- src/app/pages/healthcare/healthcare.component.ts
+++ src/app/pages/healthcare/healthcare.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HealthcareService } from '../../services/healthcare.service';
+import { Healthcare } from '../../models/healthcare.model';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-healthcare',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './healthcare.component.html',
   styleUrl: './healthcare.component.scss'
 })
-export class HealthcareComponent {
+export class HealthcareComponent implements OnInit {
+  healthcareData: Healthcare[] = [];
 
+  constructor(private _healthcareService: HealthcareService) { }
+
+  ngOnInit() {
+    this._healthcareService.getHealthcareContent().subscribe((data: Healthcare[]) => {
+      this.healthcareData = data;
+    });
+  }
 }
