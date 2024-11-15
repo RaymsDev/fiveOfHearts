5d5c8ad3c9c3d5483905edf618d8390fa7439bf6
Rayms <rayms.dev@gmail.com>
2024-11-15 08:56:05 +0100

    wip: trying to add captcha

--- src/app/configs/app.constants.ts
+++ src/app/configs/app.constants.ts
@@ -1,4 +1,7 @@
-export const EXTERNAL_URL: Record<string, string> = {
+export const SITE_CONFIG: Record<string, string> = {
   BOOKING:
     'https://perfactive.fr/magnetiseur/saint-leger-triey/jean-luc-laffuge',
+  PHONE: 'KzMzMDY4Nzg0NjIzMA==', // Base64 encoded phone number
+  RECAPTCHA_SITE_KEY: '6Ldsxn8qAAAAAF-n49wazTU2Yof4amcd1VFNmXOq',
+  RECAPTCHA_SITE_KEY_DEV: '6Ld8sX8qAAAAAI_qMpZvNVp7ckn07QwaZuJ-ToPR',
 };
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { SITE_CONFIG } from '../../configs';
+
+declare var grecaptcha: any;
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  public captchaResponse: string | null = null;
+  public captchaSiteKey = SITE_CONFIG['RECAPTCHA_SITE_KEY_DEV'];
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      (window as any)['onCaptchaSuccess'] = (response: string) =>
+        this.onCaptchaSuccess(response);
+    }
+  }
+
+  onCaptchaSuccess(response: string): void {
+    this.captchaResponse = response; // Save the token
+    console.log('CAPTCHA resolved:', response);
+    // Send the response to your backend for verification
+  }
 }
