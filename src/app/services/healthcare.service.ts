0d14a0f619a311ec10e9085e450a961d1a1dd54e
sebastienDOC <s.dechand21@gmail.com>
2024-11-15 14:54:10 +0100

    feat(healthcare): add page with healthcare

---
+++ src/app/services/healthcare.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Healthcare } from '../models/healthcare.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HealthcareService {
+  private dataUrl = 'data/healthcare.json';
+
+  constructor(private http: HttpClient) {}
+
+  getHealthcareContent(): Observable<Healthcare[]> {
+    return this.http.get<Healthcare[]>(this.dataUrl);
+  }
+}
