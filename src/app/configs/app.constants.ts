fb3e8e39449d68f1ab4974df498f996553470ac1
sebastienDOC <s.dechand21@gmail.com>
2024-11-15 15:38:52 +0100

    feat(healthcare): add reservation button on each card

--- src/app/configs/app.constants.ts
+++ src/app/configs/app.constants.ts
@@ -7,4 +7,10 @@ export const SITE_CONFIG: Record<string, string> = {
   PHONE: 'KzMzMDY4Nzg0NjIzMA==', // Base64 encoded phone number
   RECAPTCHA_SITE_KEY: '6Ldsxn8qAAAAAF-n49wazTU2Yof4amcd1VFNmXOq',
   RECAPTCHA_SITE_KEY_DEV: '6Ld8sX8qAAAAAI_qMpZvNVp7ckn07QwaZuJ-ToPR',
+  ENERGETIC_CARE: 'https://perfactive.fr/5-de-cour/date?location=3234180&people=13548&reason=87940',
+  HOLISTIC_CARE: 'https://perfactive.fr/5-de-cour/date?location=3234180&people=13548&reason=87941',
+  HEAD_MASSAGE: 'https://perfactive.fr/5-de-cour/date?location=3234180&people=13548&reason=87942',
+  CANDLE_HOPI: 'https://perfactive.fr/5-de-cour/date?location=3234180&people=13548&reason=87944',
+  BURN_CARE: 'https://perfactive.fr/5-de-cour/date?location=3234180&people=13548&reason=87946',
+  KNAP_MASSAGE: 'https://perfactive.fr/5-de-cour/date?location=3234180&people=13548&reason=87947'
 };
