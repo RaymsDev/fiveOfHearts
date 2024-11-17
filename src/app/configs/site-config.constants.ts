7a9f38f77b1079c278b022167878a4d3238c17d6
sebastienDOC <s.dechand21@gmail.com>
2024-11-17 15:09:00 +0100

    feat(footer): add footer with links (regulations links not finalized)

--- src/app/configs/site-config.constants.ts
+++ src/app/configs/site-config.constants.ts
@@ -9,6 +9,8 @@ export const SITE_CONFIG: Record<string, string> = {
     'https://perfactive.fr/5-de-cour/date?location=3234180&people=13548&reason=87940',
   FACEBOOK:
     'https://www.facebook.com/jean.luc.laffuge.praticien.bien.etre.soin.zen',
+  LINKEDIN:
+    'https://www.linkedin.com/in/jeanluc-laffuge/',
   GOOGLE: 'https://g.co/kgs/RXuyoTf',
   GOOGLE_ANALYTICS_TAG: 'G-KJEW2CN1HE',
   GOOGLE_REVIEWS_SCRIPT: 'https://apps.elfsight.com/p/platform.js',
@@ -18,7 +20,10 @@ export const SITE_CONFIG: Record<string, string> = {
     'https://perfactive.fr/5-de-cour/date?location=3234180&people=13548&reason=87941',
   KNAP_MASSAGE:
     'https://perfactive.fr/5-de-cour/date?location=3234180&people=13548&reason=87947',
-  PHONE: 'KzMzMDY4Nzg0NjIzMA==',
+  // PHONE: 'KzMzMDY4Nzg0NjIzMA==',
+  ADDRESS : '7 rue haute, Saint-Léger-Triey, France',
+  PHONE: '06 87 84 62 30',
+  EMAIL: 'jl.laffuge@gmail.com',
   // Base64 encoded phone number
   RECAPTCHA_SITE_KEY: '6Ldsxn8qAAAAAF-n49wazTU2Yof4amcd1VFNmXOq',
   RECAPTCHA_SITE_KEY_DEV: '6Ld8sX8qAAAAAI_qMpZvNVp7ckn07QwaZuJ-ToPR',
