994cfdef7a58bcddab9f41215b19a4a35f8846b1
Rayms <rayms.dev@gmail.com>
2024-12-13 13:24:48 +0100

    feat(healthcare): update model and booking urls
    * update json-ld

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { SITE_CONFIG } from '../../configs';
-import { Healthcare } from '../../models/healthcare.model';
+import { Component, Input } from '@angular/core';
+import { Healthcare, Product } from '../../models/healthcare.model';
 import { ButtonComponent } from '../button/button.component';
 
 @Component({
@@ -11,47 +10,42 @@ import { ButtonComponent } from '../button/button.component';
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
   @Input() healthcareItem!: Healthcare;
   @Input() selectedOffice: string = '';
-  bookingUrl: string = '';
   isExpanded: boolean = false;
-
-  ngOnInit() {
-    this.bookingUrl = this.getBookingUrl();
-  }
 
   toggleDescription() {
     this.isExpanded = !this.isExpanded;
   }
 
-  getCabinets(prices: { [cabinet: string]: string }): string[] {
-    return Object.keys(prices).filter((cabinet) => prices[cabinet] !== 'NO');
+  getOffices(products: { [office: string]: Product }): string[] {
+    return Object.keys(products);
+  }
+
+  getPrice(office: string): string {
+    const product = this.healthcareItem.products[office];
+
+    if (typeof product.price === 'string') {
+      return product.price;
+    }
+
+    const currency =
+      product.price.currency === 'EUR' ? '€' : product.price.currency;
+
+    return `${product.price.value} ${currency}`;
+  }
+
+  getBookingUrl(office?: string): string {
+    if (!office) {
+      const firstOffice = Object.keys(this.healthcareItem.products)[0];
+      return this.healthcareItem.products[firstOffice].bookingUrl;
+    }
+
+    return this.healthcareItem.products[office].bookingUrl;
   }
 
   get duration(): string {
     return `${this.healthcareItem.duration.value} ${this.healthcareItem.duration.unitText}`;
-  }
-
-  /**
-   * TODO: This method should be refactored to put the booking URL in the healthcare model and in the json data
-   */
-  private getBookingUrl(): string {
-    switch (this.healthcareItem.title) {
-      case 'Soins énergétiques':
-        return SITE_CONFIG['ENERGETIC_CARE'];
-      case 'Massage Holistique':
-        return SITE_CONFIG['HOLISTIC_CARE'];
-      case 'Micro massage crânien':
-        return SITE_CONFIG['HEAD_MASSAGE'];
-      case "Bougies d'oreille Hopi":
-        return SITE_CONFIG['CANDLE_HOPI'];
-      case 'Barrage des brûlures':
-        return SITE_CONFIG['BURN_CARE'];
-      case 'Massage Knap':
-        return SITE_CONFIG['KNAP_MASSAGE'];
-      default:
-        return SITE_CONFIG['BOOKING'];
-    }
   }
 }
